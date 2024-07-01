5aaf4563f81346330f9317a00dad31269b28df46
madonuko <madonuko@outlook.com>
2024-07-01 12:55:33 +0800

    docs: `Mount::list()`

--- src/mountpoints.rs
+++ src/mountpoints.rs
@@ -16,6 +16,28 @@ pub struct Mount {
 }
 
 impl Mount {
+    /// List out all mountpoints and populate all fields.
+    ///
+    /// # Errors
+    /// Since this function depends on the existance of `/proc/mounts`, failures to open the file
+    /// will cause [`crate::LsblkError::ReadFile`].
+    ///
+    /// # Caveats
+    /// If for some reason `/proc/mounts` is not formatted properly, the iterator will skip those
+    /// lines. This includes
+    /// - trailing whitespace
+    /// - `fs_freq` and `fs_passno` (which are the last 2 fields on each line) not set to 0
+    /// - not separating the fields with only 1 single space (`' '`)
+    ///
+    /// For more information, visit [`proc_pid_mounts(5)`](https://man.archlinux.org/man/proc_mounts.5.en).
+    ///
+    /// # Examples
+    /// ```
+    /// for m in lsblk::Mount::list()? {
+    ///     println!("{} mounted at {}", m.device, m.mountpoint.display());
+    /// }
+    /// # Ok::<(), lsblk::LsblkError>(())
+    /// ```
     pub fn list() -> Res<impl Iterator<Item = Mount>> {
         Ok(std::io::BufReader::new(
             std::fs::File::open(PathBuf::from("/proc/mounts"))
